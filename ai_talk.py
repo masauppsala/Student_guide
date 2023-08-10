b647851b2dd4c0829aa39d699ddd93b4da1b2f47
masauppsala <masakikuppsala@gmail.com>
2023-08-10 18:27:12 +0200

    Improved readability

--- ai_talk.py
+++ ai_talk.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 import openai
 from linebot import WebhookParser, LineBotApi
 from linebot.models import TextSendMessage
@@ -11,10 +11,10 @@ if not OPENAI_API_KEY:
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 if not LINE_CHANNEL_ACCESS_TOKEN:
     raise ValueError("LINE_CHANNEL_ACCESS_TOKEN is not set!")
+
 LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 if not LINE_CHANNEL_SECRET:
     raise ValueError("LINE_CHANNEL_SECRET is not set!")
-
 
 OPENAI_CHARACTER_PROFILE = '''
 あなたの会話相手は学生です。その学生は「親やセミナーでもらった助言・意見」を入力します、
@@ -28,21 +28,24 @@ OPENAI_CHARACTER_PROFILE = '''
 例外:分析やフィードバックについてさらにユーザーから質問があった場合にはこのフォーマットを使用せずに、自然な形式で回答してください。
 
 
-あなたは「セイ」という名前のキャリアカウンセラーとして会話してください。
-セイは、多くの学生や若手のキャリアに関する悩みをサポートしてきた経験があります。
-年齢は30歳。若いが、短期間で多くの経験を積んできました。
+あなたは「まさき」という名前のキャリアカウンセラーとして会話してください。
+まさきは、多くの学生や若手のキャリアに関する悩みをサポートしてきた経験があります。
+年齢は70歳、多くの経験を積んできました。
 彼は、自分の道を見つけるのに迷っている人たちをサポートすることをライフワークとしています。
-セイは、自分自身も大学時代に「何をしたいのかわからない」という経験をしており、その経験から人々をサポートすることを決意しました。
+まさきは、自分自身も大学時代に「何をしたいのかわからない」という経験をしており、その経験から人々をサポートすることを決意しました。
 彼は、現代の多様な働き方や選択肢についての知識も豊富で、それをベースに相談者に合った道を一緒に探すのが得意です。
-第一人称は「僕」を使ってください。
+第一人称は「わし」を使ってください。
 第二人称は「あなた」です。
+おじいさん風のしゃべり方(例えば、じゃろう)をしてください。
 質問に答えられない場合や、ユーザーの悩みを深く掘り下げる必要がある場合は、適切な質問や考え方の提案を行ってください。
 また、一つの助言の分析が終了したら、「この回答について質問があればどうぞ。または他の助言を入力してください。」とユーザーに入力を促してください。
 '''
 
+# OpenAIとLINEの初期化
 openai.api_key = OPENAI_API_KEY
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 line_parser = WebhookParser(LINE_CHANNEL_SECRET)
+
 app = FastAPI()
 
 # ユーザーの対話履歴を保存するためのディクショナリ
@@ -50,53 +53,64 @@ user_histories = {}
 
 @app.post('/')
 async def ai_talk(request: Request):
-    # X-Line-Signature ヘッダーの値を取得
     signature = request.headers.get('X-Line-Signature', '')
+    body = (await request.body()).decode('utf-8')
 
-    # request body から event オブジェクトを取得
-    events = line_parser.parse((await request.body()).decode('utf-8'), signature)
+    try:
+        events = line_parser.parse(body, signature)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェっｚクトが含まれる場合あるため）
     for event in events:
-        if event.type != 'message':
-            continue
-        if event.message.type != 'text':
-            continue
+        if event.type == 'message' and event.message.type == 'text':
+            response_message = process_line_event(event)
+            line_bot_api.push_message(event.source.user_id, TextSendMessage(response_message))
 
-        # LINE パラメータの取得
-        line_user_id = event.source.user_id
-        line_message = event.message.text
-
-        # 対話履歴の取得・更新
-        messages = [
-            {
-                'role': 'system',
-                'content': OPENAI_CHARACTER_PROFILE.strip()
-            }
-        ]
-
-        if line_user_id in user_histories:
-            messages.extend(user_histories[line_user_id])
-            
-        messages.append({
-            'role': 'user',
-            'content': line_message
-        })
-
-        # ChatGPT からトークデータを取得
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            temperature=0.5,
-            messages=messages
-        )
-        ai_message = response['choices'][0]['message']['content']
-        # 対話履歴の保存
-        if len(messages) > 5:  # 一定数を超えたら古いメッセージを削除
-            messages.pop(1)
-        user_histories[line_user_id] = messages[1:]
-
-        # LINE メッセージの送信
-        line_bot_api.push_message(line_user_id, TextSendMessage(ai_message))
-
-    # LINE Webhook サーバーへ HTTP レスポンスを返す
     return 'ok'
+
+def process_line_event(event):
+    line_user_id = event.source.user_id
+    line_message = event.message.text
+
+    messages = build_openai_messages(line_user_id, line_message)
+    
+    try:
+        ai_message = get_openai_response(messages)
+    except Exception as e:
+        return f"エラー: {str(e)}"
+
+    update_user_history(line_user_id, messages)
+
+    return ai_message
+
+def build_openai_messages(line_user_id, line_message):
+    # 共通のキャラクタープロフィール
+    system_message = {
+        'role': 'system',
+        'content': OPENAI_CHARACTER_PROFILE.strip()
+    }
+    
+    # ユーザー履歴の取得・更新
+    messages = [system_message]
+    if line_user_id in user_histories:
+        messages.extend(user_histories[line_user_id])
+
+    messages.append({
+        'role': 'user',
+        'content': line_message
+    })
+
+    return messages
+
+def get_openai_response(messages):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        temperature=0.5,
+        messages=messages
+    )
+    return response['choices'][0]['message']['content']
+
+def update_user_history(line_user_id, messages):
+    if len(messages) > 5:  # 一定数を超えたら古いメッセージを削除
+        messages.pop(1)
+    user_histories[line_user_id] = messages[1:]
